d0b348f17ad1f5f9d6242dcbacfb1428397562e0
Yuri Sousa <79167904+Nop-Dev@users.noreply.github.com>
2023-12-13 14:48:23 -0300

    feat: :sparkles: Segundo mini-projeto concluído (sum2numbers)

---
+++ project02-sum2numbers/script.js
@@ -0,0 +1,8 @@
+alert("Vamos somar dois números...");
+
+let number1 = prompt("Digite o primeiro número...");
+let number2 = prompt("Agora digite o segundo número...");
+
+let sum = Number(number1) + Number(number2);
+
+alert(`A soma de ${number1} + ${number2} é = ${sum}`)
